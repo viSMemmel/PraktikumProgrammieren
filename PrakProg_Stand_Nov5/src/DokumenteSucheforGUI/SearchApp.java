308530dc10c420e943bdb8087e0e883a96a485ae
viSMemmel <stefan.memmel@hof-university.de>
2015-11-08 16:07:41 +0100

    Stand Nov07

---
+++ PrakProg_Stand_Nov5/src/DokumenteSucheforGUI/SearchApp.java
@@ -0,0 +1,23 @@
+package DokumenteSucheforGUI;
+
+import DokumenteSuche.SearchAlt;
+
+public class SearchApp {
+	/*1.12 Zwischenpräseentation
+	 * wird eigentlich nicht mehr benötigt, da die Index App automatisch eine Suche instanziertt
+	 * 
+	 * */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SearchAlt s  =  new SearchAlt("C:/Users/Administrator/Desktop/Semester4/test","du");
+		try{
+		s.find();
+		System.out.println("Funtst");
+	}catch(Exception e){
+		System.out.println("fehler !!!");
+		e.printStackTrace();
+	
+	}
+	}
+}
---
+++ PrakProg_Stand_Nov5/src/Index/FileReaderApp.java
@@ -0,0 +1,11 @@
+package Index;
+
+public class FileReaderApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FReader fr = new FReader("C:/Users/Administrator/Desktop/Semester4/test/test3.txt");
+		System.out.println(fr.getText());
+	}
+
+}
